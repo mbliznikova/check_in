ddf509b1d1af3410194f23d1a893e03ac49e118e
Margarita Bliznikova <captainderteufel@gmail.com>
2025-05-07 16:20:32 -0700

    Update ConfirmationDetails component

--- components/ConfirmationDetails.tsx
+++ components/ConfirmationDetails.tsx
@@ -32,25 +32,106 @@ const ConfirmationDetails = ({
     date,
     classes,
 }: AttendanceType) => {
-
+    const colorScheme = useColorScheme();
     return (
-        <View>
-            <Text>{date}</Text>
-            <FlatList
-                data={Object.entries(classes)}
-                keyExtractor={([classId, _classInfo]) => classId.toString()}
-                renderItem={( {item} ) => {
-                    const [classId, classInfo] = item;
-                    return (
-                        <ClassName
-                            id={Number(classId)}
-                            name={classInfo.name}
-                        />
-                    );
-                }}
-            />
-        </View>
+        <SafeAreaView style={styles.appContainer}>
+            <View style={[styles.contentContainer, styles.bigFlex]}>
+            <ScreenTitle titleText={date}></ScreenTitle>
+                <FlatList
+                    data={Object.entries(classes)}
+                    keyExtractor={([classId, _classInfo]) => classId.toString()}
+                    renderItem={( {item} ) => {
+                        const [classId, classInfo] = item;
+                        return (
+                            <View>
+                                <ClassName
+                                    id={Number(classId)}
+                                    name={classInfo.name}
+                                />
+                                <View style={styles.spaceBetweenRow}>
+                                    <Text style={[colorScheme === 'dark' ? styles.lightColor : styles.darkColor]}>
+                                        Student
+                                    </Text>
+                                    <Text style={[colorScheme === 'dark' ? styles.lightColor : styles.darkColor]}>
+                                        Did actually show up?
+                                    </Text>
+                                </View>
+                            </View>
+                        );
+                    }}
+                />
+            </View>
+
+            <View style={[styles.confirmButtonContainer, styles.smallFlex]}>
+                    <Pressable
+                        style={styles.confirmButton}
+                        // disabled={!ifStudentsToConfirm}
+                        onPress={() => {
+                            // sendConfirmation();
+                            // alert('Students have been confirmed');
+                        }}
+                    >
+                        <Text>Confirm</Text>
+                    </Pressable>
+            </View>
+
+            <View style={styles.separator} />
+
+        </SafeAreaView>
     );
 };
+
+const styles = StyleSheet.create({
+    appContainer: {
+        flex: 1,
+    },
+    bigFlex: {
+        flex: 5,
+    },
+    smallFlex: {
+        flex: 1,
+    },
+    contentContainer: {
+        paddingHorizontal: 16,
+    },
+    separator: {
+        height: 1,
+        backgroundColor: 'gray',
+        marginVertical: 10,
+    },
+    spaceBetweenRow: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingVertical: 10,
+        paddingHorizontal: 10,
+    },
+    confirmButtonContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingBottom: 20,
+    },
+    confirmButton: {
+        paddingVertical: 5,
+        paddingHorizontal: 10,
+        marginVertical: 10,
+        borderRadius: 15,
+        backgroundColor: 'blue',
+    },
+    leftText: {
+        fontWeight: '600',
+    },
+    rightText: {
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+    darkColor: {
+        color: 'black',
+    },
+    lightColor: {
+        color: 'white',
+    },
+})
+
 
 export default ConfirmationDetails;
