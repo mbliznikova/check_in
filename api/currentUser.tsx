0573c82805ea3d817bfd4277e64a41e14d3f5dbe
Margarita Bliznikova <captainderteufel@gmail.com>
2026-01-12 10:52:55 -0800

    Move fetchCurrentUser and isValidCurrentUserResponse from components/School.tsx to api/currentUser.tsx #39

---
+++ api/currentUser.tsx
@@ -0,0 +1,37 @@
+import { useApi } from "@/api/client";
+
+const isValidCurrentUserResponse = (responseData: any): boolean => {
+    return (
+        typeof responseData === 'object' &&
+        responseData !== null &&
+        'role' in responseData
+    );
+}
+
+export const fetchCurrentUser = async () => {
+    const { apiFetch } = useApi();
+    try {
+        const response = await apiFetch("/me/",
+            { method: "GET" }
+        );
+
+        if (response.ok) {
+            const responseData = await response.json();
+            if (
+                isValidCurrentUserResponse(responseData)
+            ) {
+                const userRole: string = responseData.response.role;
+                return userRole;
+            } else {
+                console.warn('Function fetchCurrentUser. The response from backend is NOT valid! '  + JSON.stringify(responseData));
+            }
+        }
+
+        console.warn("Function fetchCurrentUser. Request was unsuccessful: ", response.status, response.statusText);
+        return null;
+
+    } catch(err) {
+        console.error("Error while fetching the current user: ", err);
+        return null;
+    }
+}
