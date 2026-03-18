e6807cae488f419973d50eedea4a2a12c771d228
Margarita Bliznikova <captainderteufel@gmail.com>
2026-03-17 18:23:52 -0700

    Add the button to create a school #49

--- components/SchoolManagement.tsx
+++ components/SchoolManagement.tsx
@@ -1,14 +1,56 @@
-import { SafeAreaView } from "react-native";
+import { Pressable, SafeAreaView, View, Text, StyleSheet, } from "react-native";
+
+import { useThemeTextStyle } from '@/hooks/useThemeTextStyle';
 import { Header } from "./Header";
 import ScreenTitle from "./ScreenTitle";
 
 const SchoolManagement = () => {
+    const textStyle = useThemeTextStyle();
+
+    const renderCreateSchool = () => {
+        return (
+            <View style={styles.container}>
+                <Pressable
+                    onPress = {() => {}}
+                    style={({ pressed }) => [
+                        styles.button,
+                        pressed ? styles.primaryButtonPressed : styles.primaryButtonUnpressed
+                    ]}
+                >
+                    <Text style={[textStyle]}>+  Create School</Text>
+                </Pressable>
+            </View>
+        );
+    };
+
     return (
         <SafeAreaView style={{ flex: 1 }}>
             <Header/>
             <ScreenTitle titleText='School Management'></ScreenTitle>
+            {renderCreateSchool()}
         </SafeAreaView>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        paddingHorizontal: 20,
+        paddingVertical: 20,
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+      },
+    button: {
+        paddingVertical: 5,
+        paddingHorizontal: 10,
+    },
+    primaryButtonPressed: {
+        backgroundColor: '#c2c0c0',
+        borderRadius: 8,
+    },
+    primaryButtonUnpressed: {
+        backgroundColor: 'blue',
+        borderRadius: 8,
+    },
+});
 
 export default SchoolManagement;
